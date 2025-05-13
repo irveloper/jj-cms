2d7074f995d66fb508df61edb9063c054eea36da
Irving Caamal <irvv.17@gmail.com>
2025-05-13 10:05:55 -0500

    fix(client): deploy

--- src/api/social-link/services/social-link.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * social-link service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::social-link.social-link');
